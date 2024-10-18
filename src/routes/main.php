84bd0a2e1d0725cbf62cf270edc3c54bb4f65b42
PedroLourenco18 <pedroedu2007@gmail.com>
2024-10-18 18:34:03 -0300

    feat: add routes/main.php and create the routes
    This commit adds the main routes file, and creates
    the 404, products, user, authentication and OAuth
    routes. Also include routes/main.php in index.php

---
+++ src/routes/main.php
@@ -0,0 +1,33 @@
+<?php
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+try{
+    SimpleRouter::setDefaultNamespace("src\Controllers");   
+
+    //ERROR 404 ROUTE
+    SimpleRouter::get("prodsphp/404", "NotFoundController@error404");    
+
+    //PRODUCTS ROUTES
+    SimpleRouter::get("prodsphp/products", "ProductsController@list");
+    SimpleRouter::get("prodsphp/products/{id}", "ProductsController@search");
+    SimpleRouter::post("prodsphp/products", "ProductsController@create");
+    SimpleRouter::put("prodsphp/products/{id}", "ProductsController@update");
+    SimpleRouter::delete("prodsphp/products/{id}", "ProductsController@delete");
+
+    //USER ROUTES
+    SimpleRouter::get("prodsphp/user", "UserController@search");
+    SimpleRouter::post("prodsphp/user", "UserController@signUp");
+    SimpleRouter::put("prodsphp/user", "UserController@update");
+    SimpleRouter::delete("prodsphp/user", "UserController@delete");
+
+    //AUTHENTICATION ROUTE
+    SimpleRouter::post("prodsphp/user/login", "UserController@login");
+
+    //OAUTH 2.0 ROUTES
+    SimpleRouter::get("prodsphp/oauth/google", "OauthController@google");
+
+    SimpleRouter::start();
+}catch(Pecee\SimpleRouter\Exceptions\NotFoundHttpException $e){
+    header("Location: http://localhost/rest/404");
+}
