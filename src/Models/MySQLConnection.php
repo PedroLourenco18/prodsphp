b9af4198561f7027a120acb3de2b68d812fd5ac2
PedroLourenco18 <pedroedu2007@gmail.com>
2024-10-22 19:43:49 -0300

    fix: change database credencials to .env variables
    this commit fixes a security flaw by adding a
    .env file
    
    This commit adds a .env file and the vlucas
    framework to load it, and replace the database
    credencials for variables in the .env file

--- src/Models/MySQLConnection.php
+++ src/Models/MySQLConnection.php
@@ -4,11 +4,15 @@ namespace src\Models;
 
 use PDO;
 use src\Interfaces\DbConnectionInterface;
+use Dotenv\Dotenv;
+
+$dotenv =  Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 class MySQLConnection implements DbConnectionInterface{
     private PDO $connection;
     public function __construct(){
-        $this->connection = new PDO('mysql:host=localhost;dbname=api', "root", "");
+        $this->connection = new PDO("mysql:host={$_ENV["DB_HOST"]};dbname={$_ENV["DB_NAME"]}", $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]);
     }
     public function fetch(string $sql, array $params = null){
         $stmt = $this->connection->prepare($sql);
