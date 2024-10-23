f980766e9b88137cb4bb811baaa301cdc6993ccc
PedroLourenco18 <pedroedu2007@gmail.com>
2024-10-23 19:49:23 -0300

    feat: create user and products models

---
+++ src/Models/ProductsModel.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace src\Models;
+
+use src\Interfaces\ProductsModelInterface;
+use src\Interfaces\DbConnectionInterface;
+
+class ProductsModel implements ProductsModelInterface{
+    private $con;
+    public function __construct(DbConnectionInterface $dbConnection){
+        $this->con = $dbConnection;
+    } 
+
+    public function read(){
+        $sql = "SELECT * FROM products";
+
+        return $this->con->fetchAll($sql);
+    }
+
+    public function searchById(int $id){
+        $sql = "SELECT * FROM products WHERE id = {$id}";
+
+        return $this->con->fetch($sql);
+    }
+
+    public function create(array $data){
+        $sql = "INSERT INTO products (`id`, `brand`, `name`, `price`) VALUES (NULL, ?, ?, ?);";
+
+        $this->con->execute($sql, [
+            $data["brand"],
+            $data["name"],
+            $data["price"]
+        ]);
+    }
+
+    public function update(int $id, array $data){
+        $columns = "";
+        $values = [];
+
+        foreach($data as $key => $value){
+            $columns = $key." =?, ";
+            array_push($values, $value);
+        }
+        $columns = substr($columns, 0, -2);
+        
+        $sql = "UPDATE products SET {$columns} WHERE id = {$id}";
+
+        $this->con->execute($sql, $values);
+    }
+
+    public function delete(int $id){
+        $sql = "DELETE FROM products WHERE id = {$id}";
+
+        $this->con->execute($sql);
+    }
+}
---
+++ src/Models/UserModel.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace src\Models;
+
+use src\Interfaces\UserModelInterface;
+use src\Interfaces\DbConnectionInterface;
+
+class UserModel implements UserModelInterface{
+    private $con;
+    public function __construct(DbConnectionInterface $dbConnection){
+        $this->con = $dbConnection;
+    }
+
+    public function searchById(int $id){
+        $sql = "SELECT * FROM users WHERE id = {$id}";
+
+        return $this->con->fetch($sql);
+    }
+
+    public function searchByEmail(string $email){
+        $sql = "SELECT * FROM users WHERE email = {$email}";
+
+        return $this->con->fetch($sql);
+    }
+
+    public function create(array $data){
+        $sql = "INSERT INTO users (`id`, `name`, `email`, `role`, `password`) VALUES (NULL, ?, ?, ?, ?);";
+
+        $this->con->execute($sql, [
+            $data["name"],
+            $data["email"],
+            $data["role"],
+            $data["password"]
+        ]);
+    }
+
+    public function update(int $id, array $data){
+        $columns = "";
+        $values = [];
+        
+        foreach($data as $key => $value){
+            $columns = $key." =?, ";
+            array_push($values, $value);
+        }
+        $columns = substr($columns, 0, -2);
+        
+        $sql = "UPDATE users SET {$columns} WHERE id = {$id}";
+
+        $this->con->execute($sql, $values);
+    }
+
+    public function delete(int $id){
+        $sql = "DELETE FROM users WHERE id = {$id}";
+
+        $this->con->execute($sql);
+    }
+}
