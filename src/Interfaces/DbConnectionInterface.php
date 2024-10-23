d81540af114327d30ab85407b33f74bb8ca9414d
PedroLourenco18 <pedroedu2007@gmail.com>
2024-10-23 19:28:06 -0300

    feat: add some functions in connection class
    This commit adds the functions execute,
    getLastInsertId and getRowCount int the connection
    interface and sql connection class

--- src/Interfaces/DbConnectionInterface.php
+++ src/Interfaces/DbConnectionInterface.php
@@ -5,4 +5,7 @@ namespace src\Interfaces;
 interface DbConnectionInterface{
     public function fetch(string $sql, array $params = null);
     public function fetchAll(string $sql, array $params = null);
+    public function execute(string $sql, array $params = null);
+    public function getLastInsertId();
+    public function getRowCount();
 }
--- src/Models/MySQLConnection.php
+++ src/Models/MySQLConnection.php
@@ -11,22 +11,33 @@ $dotenv->load();
 
 class MySQLConnection implements DbConnectionInterface{
     private PDO $connection;
+    private \PDOStatement $stmt;
     public function __construct(){
         $this->connection = new PDO("mysql:host={$_ENV["DB_HOST"]};dbname={$_ENV["DB_NAME"]}", $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"]);
     }
     public function fetch(string $sql, array $params = null){
-        $stmt = $this->connection->prepare($sql);
+        $this->execute($sql, $params);
 
-        $stmt->execute($params);
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     public function fetchAll(string $sql, array $params = null){
-        $stmt = $this->connection->prepare($sql);
+        $this->execute($sql, $params);
 
-        $stmt->execute($params);
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function execute(string $sql, array $params = null){
+        $this->stmt = $this->connection->prepare($sql);
+
+        $this->stmt->execute($params);
+    }
+
+    public function getLastInsertId(){
+        return $this->connection->lastInsertId();
+    }
+
+    public function getRowCount(){
+        $this->stmt->rowCount();
     }
 }
